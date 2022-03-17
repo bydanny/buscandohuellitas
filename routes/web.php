2ff869d31dc968c1860a04a88c0b31f550b10424
Daniel Ramirez <danny_1211@outlook.com>
2022-03-16 22:32:16 -0600

    Index Template Faltan archivos del Template

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('bienvenido');
+});
+*/
+
+Route::get('/', function () {
+    return view('bienvenido');
 });
 
 Auth::routes();
