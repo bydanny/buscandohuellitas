3e00cc73833d61b714528777a5af2b46093d4216
Daniel Ramirez <danny_1211@outlook.com>
2022-03-24 21:39:19 -0600

    Trabajando en Layouts

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,207 @@
+
+    <section>
+          <div class="shell shell-wide">
+            <div class="range text-sm-left">
+              <div class="cell-xl-10 cell-md-9">
+                <div class="jumbotron" style="background: url('../vendors/images/index-01.jpg') center; background-size: cover;">
+                  <h1>English <span class="text-white"> Foxhound</span><span class="reveal-xs-block"> <span class="icon icon-lg icon-circle"><span>&</span></span><span class="text-middle">Shetland </span><span class="text-middle text-white"> Sheepdog</span></span></h1><a class="link link-white" href="#">dog breeder website</a>
+                </div>
+              </div>
+              <div class="cell-md-3 cell-xl-2 cell-xs-middle offset-top-40 offset-md-top-0">
+                <div class="range">
+                  <div class="cell-md-12 cell-sm-4">
+                    <div class="inset-xl-left-40">
+                      <h4>follow us</h4>
+                      <ul class="list-inline offset-top-20 offset-md-top-40">
+                        <li><a class="icon icon-gray icon-circle icon-xs fa-facebook" href="#"></a></li>
+                        <li><a class="icon icon-gray icon-circle icon-xs fa-twitter" href="#"></a></li>
+                        <li><a class="icon icon-gray icon-circle icon-xs fa-google-plus" href="#"></a></li>
+                        <li><a class="icon icon-gray icon-circle icon-xs fa-instagram" href="#"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="cell-md-12 cell-sm-4 offset-top-40 offset-sm-top-0 offset-md-top-80">
+                    <div class="inset-xl-left-40">
+                      <h4>contacts</h4>
+                      <div class="offset-top-20 offset-md-top-40" href="../fonts/material-icons-phonelink_ring"><span class="text-middle icon icon-xs text-primary inset-right-10 material-icons-phonelink_ring"></span><a class="extra-big" href="tel:#">+1 800 559 35 48</a></div>
+                      <div class="offset-top-10 " href="../fonts/material-icons-email"><span class="text-middle icon icon-xs text-primary inset-right-10 material-icons-email"></span><a class="big" href="mailto:#">info@demolink.org</a></div>
+                    </div>
+                  </div>
+                  <div class="cell-md-12 cell-sm-4 offset-top-40 offset-sm-top-0 offset-md-top-80">
+                    <div class="inset-xl-left-40">
+                      <h4>location</h4>
+                      <div class="offset-top-20 offset-md-top-40">
+                        <h5 class="text-sbold text-graydark">
+                          28 Jackson Blvd Ste 1020
+                          Chicago
+                          <br class='veil reveal-md-block'>
+                          IL 60604-2340
+                          
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr class="veil-md offset-top-40">
+        <section class="section-80 section-md-122 section-title section-title-left" data-title="welcome">
+          <div class="shell shell-wide">
+            <div class="page-counter"><img class="img-responsive center-block" src="../vendors/images/index-02.png" width="441" height="557" alt=""></div>
+            <div class="range range-xs-center offset-top-0 offset-md-top-65">
+              <div class="cell-md-8 cell-lg-6">
+                <h4>welcome to our website</h4>
+                <p>
+                  For many years, the English Bulldog has been named the country's most popular breed according
+                  to the Governing Council of the Dog Fancy (GCCF), and with its affectionate nature and teddy-bear good looks, it's
+                  not hard to see why. As far as personality goes, the English Bulldog is an amazing Dog.
+                </p><a class="btn btn-default" href="#">view all Dogs for sale</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div class="shell shell-wide section-92 section-lg-bottom-148 bg-img-01">
+            <hr class="divider">
+            <h4>English Bulldog Breed Profile</h4>
+            <div class="range range-xs-center text-left offset-top-40 offset-lg-top-80">
+              <div class="cell-sm-11 cell-md-10">
+                <dl class="list-terms-variant-1">
+                  <dt><span>Breed number</span></dt>
+                  <dd>40</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>Average lifespan</span></dt>
+                  <dd>15 to 20 years</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>Temperament</span></dt>
+                  <dd>Friendly and affectionate, loyal and devoted, good with children and other pets.</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>Weight</span></dt>
+                  <dd>Males 6.8 to 8.1kg; females 4.5 to 5.4kg</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>AVAILABILITY</span></dt>
+                  <dd>Easy</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>Colours</span></dt>
+                  <dd>Available in almost every colour and coat pattern you can think of.</dd>
+                </dl>
+                <dl class="list-terms-variant-1">
+                  <dt><span>Grooming</span></dt>
+                  <dd>Once a week is adequate. A simple hand groom will often remove dead hair.</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr class="veil-md offset-top-40">
+        <section class="section-80 section-md-122 section-title section-title-right" data-title="dogs for sale">
+          <div class="shell shell-wide">
+            <div class="page-counter page-counter-bottom page-counter-gray"><img class="img-responsive center-block" src="../vendors/images/index-04.png" width="441" height="377" alt=""></div>
+            <div class="range range-xs-center offset-top-0 offset-md-top-65">
+              <div class="cell-md-8 cell-lg-6">
+                <h4>CHOOSING A Dog</h4>
+                <p>
+                  Choosing between a male and a female is not an easy task, as both make wonderful companions.
+                  Differences in personality are mostly attributable to the fact that everyone has something unique about them, rather
+                   than to male vs. female differences.
+                </p>
+              </div>
+            </div>
+            <!-- Owl Carousel-->
+            <div class="owl-carousel offset-top-40 offset-lg-top-96 owl-dots-modile" data-dots="true" data-items="1" data-sm-items="2" data-md-items="3" data-margin="30" data-lg-margin="84" data-mouse-drag="false">
+              <div class="thumbnail"><img class="img-responsive" src="../vendors/images/index-05.jpg" width="480" height="321" alt="">
+                <div class="caption">
+                  <h4>Date of birth: 19 Feb 2016</h4>
+                  <p class="text-graylight small text-bold text-uppercase">Delivery possible</p><a class="caption-link btn btn-default" href="#">details</a>
+                </div>
+              </div>
+              <div class="thumbnail"><img class="img-responsive" src="../vendors/images/index-06.jpg" width="480" height="321" alt="">
+                <div class="caption">
+                  <h4>Date of birth: 24 may 2012</h4>
+                  <p class="text-graylight small text-bold text-uppercase">Delivery possible</p><a class="caption-link btn btn-default" href="#">details</a>
+                </div>
+              </div>
+              <div class="thumbnail"><img class="img-responsive" src="../vendors/images/index-07.jpg" width="480" height="321" alt="">
+                <div class="caption">
+                  <h4>Date of birth: 30 Sept 2010</h4>
+                  <p class="text-graylight small text-bold text-uppercase">Delivery possible</p><a class="caption-link btn btn-default" href="#">details</a>
+                </div>
+              </div>
+            </div><a class="offset-top-20 offset-lg-top-80 btn btn-default" href="#">view all Dogs for sale</a>
+          </div>
+        </section>
+        <section class="section-bottom-80 section-lg-bottom-231">
+          <div class="shell shell-wide section-92 section-lg-bottom-148 bg-mocos">
+            <hr class="divider">
+            <h5>articles & publications</h5>
+            <div class="range range-xs-center offset-top-40 offset-lg-top-54 owl-wrapper">
+              <div class="cell-sm-11 cell-md-10">
+                <!-- Owl Carousel-->
+                <div class="owl-custom-controls-nav">
+                  <div class="owl-prev"></div>
+                  <div class="owl-next"></div>
+                </div>
+                <div class="owl-custom-nav owl-dots-modile owl-carousel" data-dots="true" data-items="1" data-mouse-drag="false">
+                  <div class="item">
+                    <div class="range range-xs-justify">
+                      <div class="cell-xl-4 cell-lg-5 text-lg-left">
+                        <h6><a class="text-dark" href="#">Problem of Puppy mortality</a></h6>
+                        <p class="small">
+                          Puppy mortality is a distressing, as well as an
+                          economic problem, which occurs to some extent in any breeding program. A study by Scott et al, 1976, of over 3,000 Puppies in over 800 litters indicated 34.5% mortality between birth and one year, which is a considerable number.
+                          
+                        </p>
+                      </div>
+                      <div class="offset-top-40 offset-lg-top-0 cell-xl-4 cell-lg-5 text-lg-right">
+                        <h6><a class="text-dark" href="#">MYTHS & FACTS ABOUT SPAYING OR NEUTERING</a></h6>
+                        <p class="small">The animal population is exploding. Each year millions of unwanted pets are born and most are treated like "living garbage" and disposed of. The primary causes of pet euthanasia are the failure of owners to have their pets spayed or neutered and animals that are abandoned or relinquished to shelters because of obedience problems.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="range range-xs-justify">
+                      <div class="cell-xl-4 cell-lg-5 text-lg-left">
+                        <h6><a class="text-dark" href="#">FACTS ABOUT DECLAWING</a></h6>
+                        <p class="small">
+                          Unlike most mammals who walk on the soles of the
+                          paws or feet, Dogs are digitigrades, which means they walk on their toes. Their back, shoulder, paw and leg joints, muscles, tendons, ligaments and nerves are naturally designed to support and distribute the Dog's weight across its toes as it walks, runs and climbs.
+                          
+                        </p>
+                      </div>
+                      <div class="offset-top-40 offset-lg-top-0 cell-xl-4 cell-lg-5 text-lg-right">
+                        <h6><a class="text-dark" href="#">COMmon Health Concerns in an Overweight Dog</a></h6>
+                        <p class="small">
+                          Concerned about how round your Dog has become, or even notice your Dog losing weight too quickly? It's easy for people to realize they've gained too much when their pants get a little snug. When it comes to a feline friend, however, its up to you to watch
+                           for warning signs that she's a little too fluffy.
+                          
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="relative">
+              <div class="page-counter page-counter-bottom-2 veil reveal-lg-block page-counter-sisal"><img class="img-responsive center-block" src="../vendors/images/index-08.png" width="378" height="361" alt=""></div>
+            </div>
+          </div>
+        </section>
+        <section> 
+          <div class="shell shell-wide">
+            <!-- RD Google Map-->
+            <div class="rd-google-map rd-google-map__model" data-zoom="12" data-x="-73.9874068" data-y="40.643180" data-styles="[{&quot;featureType&quot;:&quot;administrative.locality&quot;,&quot;elementType&quot;:&quot;all&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#2c2e33&quot;},{&quot;saturation&quot;:7},{&quot;lightness&quot;:19},{&quot;visibility&quot;:&quot;on&quot;}]},{&quot;featureType&quot;:&quot;landscape&quot;,&quot;elementType&quot;:&quot;all&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#ffffff&quot;},{&quot;saturation&quot;:-100},{&quot;lightness&quot;:100},{&quot;visibility&quot;:&quot;simplified&quot;}]},{&quot;featureType&quot;:&quot;poi&quot;,&quot;elementType&quot;:&quot;all&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#ffffff&quot;},{&quot;saturation&quot;:-100},{&quot;lightness&quot;:100},{&quot;visibility&quot;:&quot;off&quot;}]},{&quot;featureType&quot;:&quot;road&quot;,&quot;elementType&quot;:&quot;geometry&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#bbc0c4&quot;},{&quot;saturation&quot;:-93},{&quot;lightness&quot;:31},{&quot;visibility&quot;:&quot;simplified&quot;}]},{&quot;featureType&quot;:&quot;road&quot;,&quot;elementType&quot;:&quot;labels&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#bbc0c4&quot;},{&quot;saturation&quot;:-93},{&quot;lightness&quot;:31},{&quot;visibility&quot;:&quot;on&quot;}]},{&quot;featureType&quot;:&quot;road.arterial&quot;,&quot;elementType&quot;:&quot;labels&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#bbc0c4&quot;},{&quot;saturation&quot;:-93},{&quot;lightness&quot;:-2},{&quot;visibility&quot;:&quot;simplified&quot;}]},{&quot;featureType&quot;:&quot;road.local&quot;,&quot;elementType&quot;:&quot;geometry&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#e9ebed&quot;},{&quot;saturation&quot;:-90},{&quot;lightness&quot;:-8},{&quot;visibility&quot;:&quot;simplified&quot;}]},{&quot;featureType&quot;:&quot;transit&quot;,&quot;elementType&quot;:&quot;all&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#e9ebed&quot;},{&quot;saturation&quot;:10},{&quot;lightness&quot;:69},{&quot;visibility&quot;:&quot;on&quot;}]},{&quot;featureType&quot;:&quot;water&quot;,&quot;elementType&quot;:&quot;all&quot;,&quot;stylers&quot;:[{&quot;hue&quot;:&quot;#e9ebed&quot;},{&quot;saturation&quot;:-78},{&quot;lightness&quot;:67},{&quot;visibility&quot;:&quot;simplified&quot;}]}]">
+              <ul class="map_locations">
+                <li data-y="40.643180" data-x="-73.9874068">
+                  <p>9870 St Vincent Place, Glasgow, DC 45 Fr 45.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
---
+++ resources/views/layouts/partials/login-modal.blade.php
@@ -0,0 +1,12 @@
+
+<form action="" method="post" enctype="multipart/form-data">
+    <div class="modal fade text-left" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
